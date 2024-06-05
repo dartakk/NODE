31e880d18e3e8fe969e1cfca841bf1be382b6951
Francesco Damiata <damiatafrancesco@gmail.com>
2024-06-05 10:50:54 +0200

    ex2

---
+++ Ex2/repl.js
@@ -0,0 +1,17 @@
+/*
+$ node
+Welcome to Node.js v20.10.0.
+Type ".help" for more information.
+> const crypto = require('crypto')
+undefined
+> const randomID = crypto.randomUUID()
+undefined
+> console.log(randomID)
+8e3295f9-e7c9-433e-a3c8-124b43837ddc
+undefined
+>
+
+*/
+
+//Copiato dal terminale, ho preso randomUUID da qui: cosi Object.getOwnPropertyNames(crypto) (stampandolo in un log e vedendoli tutti)
+//console.log(Object.getOwnPropertyNames(crypto));
