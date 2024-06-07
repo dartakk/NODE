ec6a92f430f8ed40c6fbbdb05f90a6e49c0ee264
Francesco Damiata <damiatafrancesco@gmail.com>
2024-06-07 17:55:27 +0200

    ex4

---
+++ Ex4/ex4function.mjs
@@ -0,0 +1,5 @@
+function exMessage( message, anotmessage ) {
+    console.log(`Questo è l'${message} simile a quello di prima ma ${anotmessage}`)
+}
+
+export default exMessage;
---
+++ Ex4/importfunction.mjs
@@ -0,0 +1,3 @@
+import exMessage from "./ex4function.mjs";
+
+exMessage("esercizio numero 4", "usando ECMA modules");
